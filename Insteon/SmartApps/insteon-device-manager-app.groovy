f63bed9b6e48f8a55115a9d0d394bd6648ce88f9
HBOdR <hbodr@yahoo.com>
2018-02-03 12:29:10 -0500

    Update insteon-device-manager-app.groovy

--- Insteon/SmartApps/insteon-device-manager-app.groovy
+++ Insteon/SmartApps/insteon-device-manager-app.groovy
@@ -1,8 +1,6 @@
 /**
  *  Insteon Device Manager Smart App
  *  
- *	Author : Hugo Bonilla
- *
  *  Copyright 2018 Hugo Bonilla
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
@@ -13,7 +11,8 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
-**/
+ *
+ */
 
 definition (
   name: "Insteon Device Manager",
